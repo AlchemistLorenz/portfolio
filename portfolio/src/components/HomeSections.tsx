becd1e5649c9c1e21e963e3c108f2b5fe613c093
GucciRemyBoi <renz.wilkins@live.com>
2025-04-25 20:10:04 -0600

    closed gaps between sections

--- portfolio/src/components/HomeSections.tsx
+++ portfolio/src/components/HomeSections.tsx
@@ -12,13 +12,13 @@ export default function HomeSection() {
       <section>
         <Home />
       </section>
-      <section className="max-w-3xl mx-auto py-12 space-y-8 text-center">
+      <section className="max-w-3xl mx-auto py-16 space-y-12 text-center">
         <Timeline />
         <About />
         <Skills />
         <Link
           href="/work"
-          className="inline-block mt-8 bg-[#BB4430] hover:bg-[#EB5E28] text-white px-6 py-2 rounded transition">
+          className="inline-block -mt-12 bg-[#BB4430] hover:bg-[#EB5E28] text-white px-6 py-2 rounded transition">
           View My Portfolio →
         </Link>
       </section>
